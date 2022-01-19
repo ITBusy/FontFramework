dfedf4938309c85788f16d593c1afb390321a501
ITBusy <hoanghonvanoinhoem@gmail.com>
2022-01-19 13:26:52 +0700

    change quiz

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { QuizInterface } from '../interface/quiz.interface';
 import { SubjectInterface } from '../interface/subject.interface';
 import { Service } from '../service/service.service';
@@ -31,6 +31,19 @@ export class HomeComponent implements OnInit {
   timeValue: number = 30;
   widthValue: number = 0;
   posCurQui: number = this.randomQuiz();
+  @ViewChild('modal') modal!: ElementRef;
+  @ViewChild('info_box') info_box!: ElementRef;
+  @ViewChild('quiz_box') quiz_box!: ElementRef;
+  @ViewChild('subject_title') subject_title!: ElementRef;
+  @ViewChild('list_group') list_group!: ElementRef;
+  @ViewChild('next_btn') next_btn!: ElementRef;
+  @ViewChild('option_list') option_list!: ElementRef;
+  @ViewChild('timeText') timeText!: ElementRef;
+  @ViewChild('timeCount') timeCount!: ElementRef;
+  @ViewChild('time_line') time_line!: ElementRef;
+  @ViewChild('result_box') result_box!: ElementRef;
+  @ViewChild('score_text') score_text!: ElementRef;
+
 
   totalPages: any;
   Page = 1;
@@ -46,7 +59,7 @@ export class HomeComponent implements OnInit {
         this.quizInterface = res;
       });
   }
-  s() { }
+
   scripts() {
     const modal = this.ele.nativeElement.querySelector('.modal-quiz');
     const info_box = this.ele.nativeElement.querySelector(".info_box");
@@ -57,19 +70,19 @@ export class HomeComponent implements OnInit {
 
 
     // if exitQuiz button clicked
-    exit_btn.onclick = () => {
-      info_box.classList.remove("activeInfo"); //hide info box
-      modal.style.display = "none";
-    }
+    // exit_btn.onclick = () => {
+    //   info_box.classList.remove("activeInfo"); //hide info box
+    //   modal.style.display = "none";
+    // }
 
     // if continueQuiz button clicked
-    continue_btn.onclick = () => {
-      info_box.classList.remove("activeInfo"); //hide info box
-      quiz_box.classList.add("activeQuiz"); //show quiz box
-      subject.innerText = this.subjectName;
-      this.startTimer(30); //calling startTimer function
-      this.startTimerLine(0); //calling startTimerLine function
-    }
+    // continue_btn.onclick = () => {
+    //   info_box.classList.remove("activeInfo"); //hide info box
+    //   quiz_box.classList.add("activeQuiz"); //show quiz box
+    //   subject.innerText = this.subjectName;
+    //   this.startTimer(30); //calling startTimer function
+    //   this.startTimerLine(0); //calling startTimerLine function
+    // }
 
 
     // if restartQuiz button clicked
@@ -91,43 +104,71 @@ export class HomeComponent implements OnInit {
     //     next_btn.classList.remove("show"); //hide the next button
     // }
   }
+  clickQuit(event: any){
+    this.info_box.nativeElement.classList.remove("activeInfo");//hide info box
+    this.modal.nativeElement.style.display = "none";
+  }
 
+  continueBtn(event: any){
+    this.info_box.nativeElement.classList.remove("activeInfo"); //hide info box
+      this.quiz_box.nativeElement.classList.add("activeQuiz"); //show quiz box
+      this.subject_title.nativeElement.innerText = this.subjectName;
+      this.startTimer(30); //calling startTimer function
+      this.startTimerLine(0); //calling startTimerLine function
+  }
+
+  listSubject(event: any){
+    const ite = this.ele.nativeElement.querySelectorAll('.list_group_item');
+    // const modal = this.ele.nativeElement.querySelector('.modal-quiz');
+    // const info_box = this.ele.nativeElement.querySelector(".info_box");
+    ite.forEach((e: any) => {
+      if (e.classList.contains('modal-active')) {
+        e.classList.remove('modal-active');
+      }
+    });
+    event.target.classList.add('modal-active');
+    this.modal.nativeElement.style.display = "flex";
+    this.info_box.nativeElement.classList.add("activeInfo");
+    this.subb = event.target.getAttribute('sub');
+    this.getQuiz();
+    this.subjectName = event.target.innerHTML; 
+  }
   getSubject() {
     const ul = this.ele.nativeElement.querySelector('ul.list-group');
     this.service.getSubjectJson().subscribe(res => {
       this.subjectInterface = res;
     });
-    this.subjectInterface.forEach(e => {
-      ul.insertAdjacentHTML("afterend",
-        `<li class="list-group-item" sub="${e.Id}">${e.Name}</li>`)
-    });
-    const ite = this.ele.nativeElement.querySelectorAll('.list-group-item');
-    const modal = this.ele.nativeElement.querySelector('.modal-quiz');
-    const info_box = this.ele.nativeElement.querySelector(".info_box");
+    // this.subjectInterface.forEach(e => {
+    //   ul.insertAdjacentHTML("afterend",
+    //     `<li class="list-group-item" sub="${e.Id}">${e.Name}</li>`)
+    // });
+    // const ite = this.ele.nativeElement.querySelectorAll('.list-group-item');
+    // const modal = this.ele.nativeElement.querySelector('.modal-quiz');
+    // const info_box = this.ele.nativeElement.querySelector(".info_box");
 
-    ite.forEach((item: any) => {
-      item.onclick = () => {
-        ite.forEach((e: any) => {
-          if (e.classList.contains('modal-active')) {
-            e.classList.remove('modal-active');
-          }
-        });
-        item.classList.add('modal-active');
-        modal.style.display = "flex";
-        info_box.classList.add("activeInfo");
-        this.subb = item.getAttribute('sub');
-        this.getQuiz();
-        this.subjectName = item.innerHTML;
-      }
-    });
+    // ite.forEach((item: any) => {
+    //   item.onclick = () => {
+    //     ite.forEach((e: any) => {
+    //       if (e.classList.contains('modal-active')) {
+    //         e.classList.remove('modal-active');
+    //       }
+    //     });
+    //     item.classList.add('modal-active');
+    //     modal.style.display = "flex";
+    //     info_box.classList.add("activeInfo");
+    //     this.subb = item.getAttribute('sub');
+    //     this.getQuiz();
+    //     this.subjectName = item.innerHTML;
+    //   }
+    // });
   }
 
   nextQuiz() {
-    const next_btn = this.ele.nativeElement.querySelector("footer .next_btn");
+    // const next_btn = this.ele.nativeElement.querySelector("footer .next_btn");
     if (this.currentIndex < 9) {
       this.currentIndex++;
       this.posCurQui = this.randomQuiz();
-      next_btn.classList.remove('show');
+      this.next_btn.nativeElement.classList.remove('show');
       clearInterval(this.counter); //clear counter
       clearInterval(this.counterLine); //clear counterLine
       this.startTimer(this.timeValue); //calling startTimer function
@@ -142,13 +183,13 @@ export class HomeComponent implements OnInit {
   selectAnswer(id: number, tags: any) {
     clearInterval(this.counter); //clear counter
     clearInterval(this.counterLine); //clear counterLine
-    const option_list = this.ele.nativeElement.querySelector('.option_list');
-    const next_btn = this.ele.nativeElement.querySelector("footer .next_btn");
+    // const option_list = this.ele.nativeElement.querySelector('.option_list');
+    // const next_btn = this.ele.nativeElement.querySelector("footer .next_btn");
     let tag;
     let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
     let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
     let correctAnswer = this.quizInterface[this.posCurQui].AnswerId; //getting correct answer from array
-    const allOptions = option_list.children.length; //getting all option items
+    const allOptions = this.option_list.nativeElement.children.length; //getting all option items
 
     if (tags.tagName == 'SPAN') {
       tag = tags.parentElement;
@@ -165,82 +206,82 @@ export class HomeComponent implements OnInit {
       tag.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
 
       for (let i = 0; i < allOptions; i++) {
-        if (option_list.children[i].children[0].getAttribute('answeri') == correctAnswer) { //if there is an option which is matched to an array answer 
-          option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-          option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+        if (this.option_list.nativeElement.children[i].children[0].getAttribute('answeri') == correctAnswer) { //if there is an option which is matched to an array answer 
+          this.option_list.nativeElement.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+          this.option_list.nativeElement.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
         }
       }
     }
     for (let i = 0; i < allOptions; i++) {
-      option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+      this.option_list.nativeElement.children[i].classList.add("disabled"); //once user select an option then disabled all options
     }
-    next_btn.classList.add("show"); //show the next button if user selected any option
+    this.next_btn.nativeElement.classList.add("show"); //show the next button if user selected any option
   }
   startTimer(time: number) {
-    const option_list = this.ele.nativeElement.querySelector('.option_list');
-    const timeText = this.ele.nativeElement.querySelector(".timer .time_left_txt");
-    const timeCount = this.ele.nativeElement.querySelector(".timer .timer_sec");
+    // const option_list = this.ele.nativeElement.querySelector('.option_list');
+    // const timeText = this.ele.nativeElement.querySelector(".timer .time_left_txt");
+    // const timeCount = this.ele.nativeElement.querySelector(".timer .timer_sec");
     this.counter = setInterval(() => {
-      timeCount.textContent = time; //changing the value of timeCount with time value
+      this.timeCount.nativeElement.textContent = time; //changing the value of timeCount with time value
       time--; //decrement the time value
       if (time < 9) { //if timer is less than 9
-        let addZero = timeCount.textContent;
-        timeCount.textContent = "0" + addZero; //add a 0 before time value
+        let addZero = this.timeCount.nativeElement.textContent;
+        this.timeCount.nativeElement.textContent = "0" + addZero; //add a 0 before time value
       }
       if (time < 0) { //if timer is less than 0
         clearInterval(this.counter); //clear counter
-        timeText.textContent = "Time Off"; //change the time text to time off
-        const next_btn = this.ele.nativeElement.querySelector("footer .next_btn");
+        this.timeText.nativeElement.textContent = "Time Off"; //change the time text to time off
+        // const next_btn = this.ele.nativeElement.querySelector("footer .next_btn");
         let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
         let correctAnswer = this.quizInterface[this.posCurQui].AnswerId; //getting correct answer from array
-        const allOptions = option_list.children.length; //getting all option items
+        const allOptions = this.option_list.nativeElement.children.length; //getting all option items
 
         for (let i = 0; i < allOptions; i++) {
-          if (option_list.children[i].children[0].getAttribute('answeri') == correctAnswer) { //if there is an option which is matched to an array answer 
-            option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
-            option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+          if (this.option_list.nativeElement.children[i].children[0].getAttribute('answeri') == correctAnswer) { //if there is an option which is matched to an array answer 
+            this.option_list.nativeElement.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+            this.option_list.nativeElement.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
           }
         }
         for (let i = 0; i < allOptions; i++) {
-          option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+          this.option_list.nativeElement.children[i].classList.add("disabled"); //once user select an option then disabled all options
         }
-        next_btn.classList.add("show"); //show the next button if user selected any option
+        this.next_btn.nativeElement.classList.add("show"); //show the next button if user selected any option
       }
     }, 1000);
   }
   startTimerLine(time: number) {
-    const time_line = this.ele.nativeElement.querySelector("header .time_line");
+    // const time_line = this.ele.nativeElement.querySelector("header .time_line");
     this.counterLine = setInterval(timer, 29);
     const _this = this;
     function timer() {
       time += 1; //upgrading time value with 1
-      time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+      _this.time_line.nativeElement.style.width = time + "px"; //increasing width of time_line with px by time value
       if (time > 1100) { //if time value is greater than 549
         clearInterval(_this.counterLine); //clear counterLine
       }
     }
   }
   showResult() {
-    const info_box = this.ele.nativeElement.querySelector(".info_box");
-    const quiz_box = this.ele.nativeElement.querySelector(".quiz_box");
-    const result_box = this.ele.nativeElement.querySelector(".result_box");
+    // const info_box = this.ele.nativeElement.querySelector(".info_box");
+    // const quiz_box = this.ele.nativeElement.querySelector(".quiz_box");
+    // const result_box = this.ele.nativeElement.querySelector(".result_box");
 
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.remove("activeQuiz"); //hide quiz box
-    result_box.classList.add("activeResult"); //show result box
-    const scoreText = result_box.querySelector(".score_text");
+    this.info_box.nativeElement.classList.remove("activeInfo"); //hide info box
+    this.quiz_box.nativeElement.classList.remove("activeQuiz"); //hide quiz box
+    this.result_box.nativeElement.classList.add("activeResult"); //show result box
+    // const scoreText = result_box.querySelector(".score_text");
     if (this.userScore > 8) { // if user scored more than 3
       //creating a new span tag and passing the user score number and total question number
       let scoreTag = '<span>and congrats!<br>You got ' + this.userScore + ' Marks</span>';
-      scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
+      this.score_text.nativeElement.innerHTML = scoreTag;  //adding new span tag inside score_Text
     }
     else if (this.userScore > 5) { // if user scored more than 1
       let scoreTag = '<span>and nice!<br>You got ' + this.userScore + ' Marks</span>';
-      scoreText.innerHTML = scoreTag;
+      this.score_text.nativeElement.innerHTML = scoreTag;
     }
     else { // if user scored less than 1
       let scoreTag = '<span>and sorry!<br>You got only ' + this.userScore + ' Marks</span>';
-      scoreText.innerHTML = scoreTag;
+      this.score_text.nativeElement.innerHTML = scoreTag;
     }
   }
   complete_quiz() {
